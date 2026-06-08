170cbc4798428e9353dc8905ecf5d05b84c21099
MILAGROS NOLASCO FERNANDEZ <d44592408p@perueduca.edu.pe>
2026-06-08 04:28:59 +0000

    BOT

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,13 @@ export default function RootLayout({
           {children}
         </main>
         <Toaster />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(){if(!window.chatbase||window.chatbase("getState")!=="initialized"){window.chatbase=(...arguments)=>{if(!window.chatbase.q){window.chatbase.q=[]}window.chatbase.q.push(arguments)};window.chatbase=new Proxy(window.chatbase,{get(target,prop){if(prop==="q"){return target.q}return(...args)=>target(prop,...args)}})}const onLoad=function(){const script=document.createElement("script");script.src="https://www.chatbase.co/embed.min.js";script.id="6s0DSU2zFfK-5E0JKbwpc";script.domain="www.chatbase.co";document.body.appendChild(script)};if(document.readyState==="complete"){onLoad()}else{window.addEventListener("load",onLoad)}})();
+            `
+          }}
+        />
       </body>
     </html>
   );
